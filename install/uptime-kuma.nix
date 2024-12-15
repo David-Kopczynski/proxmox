6723282d5cbe25f6735586be5301d10b3741151e
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-12-15 19:50:40 +0100

    Added symlink to move data onto separate disk

--- install/uptime-kuma.nix
+++ install/uptime-kuma.nix
@@ -8,4 +8,13 @@
     # DATA_DIR = "/var/lib/private/uptime-kuma";
     PORT = "44301";
   };
+
+  # Manually symlink data directory as it cannot be changed
+  # /var/lib/private/uptime-kuma -> /data/uptime-kuma
+  system.activationScripts.uptime-kuma = ''
+    mkdir -p /var/lib/private
+    chmod 700 /var/lib/private
+
+    ln -s /data/uptime-kuma /var/lib/private/uptime-kuma
+  '';
 }
