112ed4ca6c9e5db59f3d226d18511652df2deba5
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-12-23 03:00:08 +0100

    Added nextcloud

---
+++ install/nextcloud.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+let
+  HOST = "cloud.davidkopczynski.com";
+  DATA = /data/nextcloud;
+in
+{
+  services.nextcloud.enable = true;
+  services.nextcloud.home = toString DATA;
+  services.nextcloud.secretFile = toString (DATA + ./secrets.json);
+  services.nextcloud.hostName = HOST;
+  services.nextcloud = {
+
+    package = pkgs.nextcloud30;
+
+    # General configuration
+    autoUpdateApps.enable = true;
+    configureRedis = true;
+    https = true;
+    maxUploadSize = "10G";
+
+    # Faster database
+    config.dbtype = "pgsql";
+    database.createLocally = true;
+
+    # Initial admin password that should be changed after first login
+    config.adminpassFile = builtins.toFile "initial-admin-password" ''
+      NextcloudAdminPassword!
+    '';
+  };
+}
