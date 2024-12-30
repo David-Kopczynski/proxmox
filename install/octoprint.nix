dd0a6dfada3e2113e34a4438e4064cbb8cf58e76
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-12-31 00:44:59 +0100

    Fixed webcam stream for octoprint

--- install/octoprint.nix
+++ install/octoprint.nix
@@ -3,6 +3,7 @@
 let
   HOST = "printer.davidkopczynski.com";
   VID0 = /dev/video0;
+  PORT = 5050;
   DATA = /data/octoprint;
 in
 {
@@ -10,10 +11,18 @@ in
   services.octoprint.stateDir = toString DATA;
   services.octoprint.extraConfig = {
 
+    # Printer configuration
+    serialDevice = "/dev/ttyACM0";
+
     # Camera configuration
     plugins.classicwebcam = {
-      snapshot = "http://localhost:5050/?action=snapshot";
-      stream = "http://localhost:5050/?action=stream";
+      snapshot = "http://localhost:${toString PORT}/?action=snapshot";
+      stream = "/webcam";
+    };
+
+    # System configuration
+    server.commands = {
+      serverRestartCommand = "systemctl restart octoprint.service";
     };
   };
 
@@ -21,6 +30,7 @@ in
   # Input is specially tuned for the Arducam 1080P Day/Night Vision USB
   services.mjpg-streamer.enable = true;
   services.mjpg-streamer.inputPlugin = "input_uvc.so -d ${toString VID0} -r 1920x1080 -n -br 0 -co 32 -sa 64 -sh 10";
+  services.mjpg-streamer.outputPlugin = "output_http.so -p ${toString PORT} -w @www@ -n";
 
   # Nginx reverse proxy to OctoPrint with port 5000
   services.nginx.virtualHosts.${HOST} = {
@@ -35,6 +45,10 @@ in
     locations."/" = {
       proxyPass = "http://localhost:${toString config.services.octoprint.port}";
       proxyWebsockets = true;
+    };
+    locations."/webcam/" = {
+      basicAuthFile = toString (DATA + "/streamer.auth");
+      proxyPass = "http://localhost:${toString PORT}/?action=stream";
     };
   };
 }
