65f476434c23e5be84d26622e5e80ec3fa062eaa
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-01-06 15:22:09 +0100

    Added tika/gotenberg to paperless

--- install/paperless.nix
+++ install/paperless.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 let
   HOST = "archive.davidkopczynski.com";
@@ -20,6 +20,28 @@ in
       # This prevents failure when PDF is signed
       invalidate_digital_signatures = true;
     };
+
+    # Add office document support
+    PAPERLESS_TIKA_ENABLED = true;
+    PAPERLESS_TIKA_ENDPOINT = "http://${config.services.tika.listenAddress}:${toString config.services.tika.port}";
+    PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://localhost:${toString config.services.gotenberg.port}";
+  };
+
+  # Required services for office document support
+  services.tika.enable = true;
+
+  services.gotenberg.enable = true;
+  services.gotenberg.chromium.disableJavascript = true;
+
+  # Fix gotenberg environment issues
+  # see https://github.com/NixOS/nixpkgs/issues/349123
+  systemd.services."gotenberg".environment = {
+    HOME = "/run/gotenberg";
+  };
+  systemd.services."gotenberg".serviceConfig = {
+    SystemCallFilter = lib.mkAfter [ "@chown" ];
+    WorkingDirectory = "/run/gotenberg";
+    RuntimeDirectory = "gotenberg";
   };
 
   # Nginx reverse proxy to Paperless with port 28981
