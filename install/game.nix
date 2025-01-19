38b5369923c34ecb1c0c74482a35eea023e30c11
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-01-19 22:17:58 +0100

    Added cloudflare tunnel and ssl to game subdomain

--- install/game.nix
+++ install/game.nix
@@ -5,48 +5,58 @@ let
   DATA = /data/game;
 in
 {
-  services.nginx.virtualHosts.${HOST}.locations = lib.mkMerge [
-    {
-      "/".root = toString DATA;
-    }
-    (
-      let
-        # Special configuration for compressed files when using Unity's WebGL build
-        disableRecommendedCompression = ''
-          brotli off;
-          gzip off;
-          zstd off;
-        '';
-        applyContentEncoding = ''
-          add_header Content-Encoding br;
-        '';
-      in
-      lib.attrsets.concatMapAttrs
-        (file: application_type: {
-          file = {
-            extraConfig = ''
-              ${
-                let
-                  configFromList = lib.strings.concatStringsSep "\n" (proxyHideHeaderLines ++ addHeaderLines);
-                  proxyHideHeaderLines = builtins.filter (lib.strings.hasPrefix "proxy_hide_header") httpToList;
-                  addHeaderLines = builtins.filter (lib.strings.hasPrefix "add_header") httpToList;
-                  httpToList = lib.strings.splitString "\n" config.services.nginx.appendHttpConfig;
-                in
-                configFromList
-              }
+  services.nginx.virtualHosts.${HOST} = {
 
-              ${disableRecommendedCompression}
-              ${applyContentEncoding}
-              ${application_type}
-            '';
-            root = toString DATA;
-          };
-        })
-        {
-          "~ .+\\.(data|symbols\\.json)\\.br$" = "default_type application/octet-stream;";
-          "~ .+\\.js\\.br$" = "default_type application/javascript;";
-          "~ .+\\.wasm\\.br$" = "default_type application/wasm;";
-        }
-    )
-  ];
+    inherit (config.cloudflare)
+      extraConfig
+      sslCertificate
+      sslCertificateKey
+      sslTrustedCertificate
+      ;
+    forceSSL = true;
+    locations = lib.mkMerge [
+      {
+        "/".root = toString DATA;
+      }
+      (
+        let
+          # Special configuration for compressed files when using Unity's WebGL build
+          disableRecommendedCompression = ''
+            brotli off;
+            gzip off;
+            zstd off;
+          '';
+          applyContentEncoding = ''
+            add_header Content-Encoding br;
+          '';
+        in
+        lib.attrsets.concatMapAttrs
+          (file: application_type: {
+            file = {
+              extraConfig = ''
+                ${
+                  let
+                    configFromList = lib.strings.concatStringsSep "\n" (proxyHideHeaderLines ++ addHeaderLines);
+                    proxyHideHeaderLines = builtins.filter (lib.strings.hasPrefix "proxy_hide_header") httpToList;
+                    addHeaderLines = builtins.filter (lib.strings.hasPrefix "add_header") httpToList;
+                    httpToList = lib.strings.splitString "\n" config.services.nginx.appendHttpConfig;
+                  in
+                  configFromList
+                }
+
+                ${disableRecommendedCompression}
+                ${applyContentEncoding}
+                ${application_type}
+              '';
+              root = toString DATA;
+            };
+          })
+          {
+            "~ .+\\.(data|symbols\\.json)\\.br$" = "default_type application/octet-stream;";
+            "~ .+\\.js\\.br$" = "default_type application/javascript;";
+            "~ .+\\.wasm\\.br$" = "default_type application/wasm;";
+          }
+      )
+    ];
+  };
 }
