41eb5944baed78c3c113275ab6b1f3a0fe46685d
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-03-04 13:21:18 +0100

    Updated mask for vfat

--- install/proxmox.nix
+++ install/proxmox.nix
@@ -30,10 +30,7 @@ in
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/boot";
     fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
+    options = [ "umask=0077" ];
   };
 
   fileSystems."/" = {
