cbbf6aa8c97b670b8142d326aeeaf45f54635407
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-06 16:05:58 +0200

    Fixed sops on remote hosts

--- sops/default.nix
+++ sops/default.nix
@@ -9,6 +9,6 @@
 
   sops.age.generateKey = true;
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  sops.age.sshKeyPaths = [ "/home/user/.ssh/id_ed25519" ];
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.gnupg.sshKeyPaths = [ ];
 }
