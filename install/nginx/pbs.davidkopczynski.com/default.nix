261a89dd4d996892d7f3e9f802d5f71b6d07541c
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-13 00:32:15 +0200

    Disabled access log for pbs

--- install/nginx/pbs.davidkopczynski.com/default.nix
+++ install/nginx/pbs.davidkopczynski.com/default.nix
@@ -8,6 +8,9 @@
     enableACME = true;
     forceSSL = true;
     locations."/" = {
+      extraConfig = ''
+        access_log off;
+      '';
       proxyPass = "https://192.168.0.170:8007/";
       proxyWebsockets = true;
     };
