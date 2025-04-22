4610fac956791c94dd49f97d3dfcf2ed9b176c78
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-22 03:59:07 +0200

    Removed unnecessary generateKey and keyFile from sops

--- sops/default.nix
+++ sops/default.nix
@@ -7,8 +7,6 @@
   sops.defaultSopsFile = sopsFile;
   sops.defaultSopsFormat = "yaml";
 
-  sops.age.generateKey = true;
-  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.gnupg.sshKeyPaths = [ ];
 }
