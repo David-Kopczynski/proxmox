8a51e45ff0b8425478f2b0886ef75b497a4184d7
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-06-30 22:24:21 +0200

    Added overlay for even newer Minecraft-Server version than available

--- install/minecraft/default.nix
+++ install/minecraft/default.nix
@@ -1,10 +1,18 @@
 { ... }:
-{ config, ... }:
+{ ... }:
 
-let
-  unstable = import <nixos-unstable> { config = config.nixpkgs.config; }; # Temporary solution for newest updates :)
-in
 {
+  # Newest Minecraft server version
+  nixpkgs.overlays = [
+    (final: prev: {
+      minecraft-server = prev.minecraft-server.override {
+        version = "1.21.7";
+        url = "https://piston-data.mojang.com/v1/objects/05e4b48fbc01f0385adb74bcff9751d34552486c/server.jar";
+        sha1 = "sha1-BeS0j7wB8Dha23S8/5dR00VSSGw=";
+      };
+    })
+  ];
+
   services.minecraft-server.enable = true;
   services.minecraft-server = {
 
@@ -17,7 +25,6 @@ in
     whitelist."StableThulium" = "73dd60b2-7616-4ccf-92b4-70898a6ac80e";
 
     # Game configuration and tweaks
-    package = unstable.minecraft-server;
     jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC";
 
     serverProperties.difficulty = 2;
