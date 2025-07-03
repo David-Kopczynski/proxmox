0c6b9b596e6d67ff80ec6c6fed72544e05d0caba
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-07-03 09:36:12 +0200

    Updated minecraft-server

--- install/minecraft/default.nix
+++ install/minecraft/default.nix
@@ -23,6 +23,7 @@
 
     # Allowed players
     whitelist."Mathisphonix" = "9cfa09b4-b042-4b15-8b96-2552d44439e1";
+    whitelist."Ra_Orkon" = "63383045-811e-4827-ac76-e1437cdf58c5";
     whitelist."StableThulium" = "73dd60b2-7616-4ccf-92b4-70898a6ac80e";
     whitelist."Terminal79" = "8e4f7d49-5b4d-4f80-af46-59ff53e0e821";
     whitelist."TheChaosBrain" = "cde2ebf7-3c01-4420-ac34-29f05f5c1656";
@@ -33,6 +34,7 @@
     serverProperties.difficulty = 2;
     serverProperties.enforce-whitelist = true;
     serverProperties.motd = "CHICKEN JOCKEY!";
+    serverProperties.view-distance = 16;
     serverProperties.white-list = true;
   };
 }
