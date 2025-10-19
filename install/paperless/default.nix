717b24516e0f4d590c91d76df074c7248972278b
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-10-19 17:29:05 +0200

    Modernized Paperless with host address, tika auto-configuration and better DB performance

--- install/paperless/default.nix
+++ install/paperless/default.nix
@@ -3,15 +3,15 @@
 
 {
   services.paperless.enable = true;
-  services.paperless.settings = {
+  services.paperless = {
 
     # General configuration
-    PAPERLESS_URL = "https://${domain}";
-    PAPERLESS_TRUSTED_PROXIES = "127.0.0.0/8, 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16";
+    settings.PAPERLESS_URL = "https://${domain}";
+    settings.PAPERLESS_TRUSTED_PROXIES = "127.0.0.0/8, 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16";
 
     # Custom settings for my optimal setup
-    PAPERLESS_OCR_LANGUAGE = "deu+eng";
-    PAPERLESS_OCR_USER_ARGS = {
+    settings.PAPERLESS_OCR_LANGUAGE = "deu+eng";
+    settings.PAPERLESS_OCR_USER_ARGS = {
 
       # PDF optimization
       optimize = 1;
@@ -22,17 +22,11 @@
     };
 
     # Add office document support
-    PAPERLESS_TIKA_ENABLED = true;
-    PAPERLESS_TIKA_ENDPOINT = "http://${config.services.tika.listenAddress}:${toString config.services.tika.port}";
-    PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://127.0.0.1:${toString config.services.gotenberg.port}";
+    configureTika = true;
+
+    # Faster database
+    database.createLocally = true;
   };
-
-  # Required services for office document support
-  services.tika.enable = true;
-  services.tika.listenAddress = "127.0.0.1";
-
-  services.gotenberg.enable = true;
-  services.gotenberg.chromium.disableJavascript = true;
 
   # Nginx reverse proxy to Paperless with port 28981
   imports = [ ../nginx/proxy-pass.websockets.nix ];
@@ -46,7 +40,7 @@
 
         client_max_body_size 1G;
       '';
-      proxyPass = "http://127.0.0.1:${toString config.services.paperless.port}/";
+      proxyPass = "http://${config.services.paperless.address}:${toString config.services.paperless.port}/";
       proxyWebsockets = true;
     };
   };
