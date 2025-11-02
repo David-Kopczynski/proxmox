b0f56d3f22a4fed55f1014455c503d611d00e819
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-11-02 21:54:32 +0100

    Added logging to endlessh

--- install/nginx/default.nix
+++ install/nginx/default.nix
@@ -94,6 +94,7 @@ in
 
   # Trap SSH attacks (external 22 forwarded to internal 2222)
   services.endlessh.enable = true;
+  services.endlessh.extraOptions = [ "-v" ];
   services.endlessh.openFirewall = true;
 
   # Secrets
