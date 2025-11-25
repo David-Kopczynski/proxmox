a357eb71b4645fda0344bdf8a26099c25b61c413
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-11-25 20:09:45 +0100

    Fixed local link bug

--- default.nix
+++ default.nix
@@ -76,9 +76,6 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPLqP71iBRAFd7OFIjlkN6yGEr++G5eRDJ+U57R9f8e user@nixos"
   ];
 
-  # Prevent local link hostname resolution
-  networking.enableIPv6 = false;
-
   # Update locale and timezone
   console.keyMap = "de";
   time.timeZone = "Europe/Berlin";
