82222dd7800de18b9b7cd1fdb76bc1794d98c44f
David E. C. Kopczynski <david.kop.dk@gmail.com>
2026-03-19 20:43:21 +0100

    fix(all): ipv6 resolver not working correctly (see nginx logs without)

--- install/nginx/home.davidkopczynski.com/default.nix
+++ install/nginx/home.davidkopczynski.com/default.nix
@@ -13,7 +13,7 @@
         proxy_read_timeout 3600s;
         proxy_send_timeout 3600s;
       '';
-      proxyPass = "http://homeassistant:8123/";
+      proxyPass = "http://192.168.0.136:8123/";
       proxyWebsockets = true;
     };
   };
