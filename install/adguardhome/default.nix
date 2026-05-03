791f3f7c844b3bfa52edaf9686bc832b08304961
David E. C. Kopczynski <david.kop.dk@gmail.com>
2026-05-03 20:58:29 +0200

    chore: configured adguardhome

--- install/adguardhome/default.nix
+++ install/adguardhome/default.nix
@@ -1,12 +1,56 @@
 { ... }:
 { config, ... }:
 
+let
+  GATE = "10.4.0.1";
+in
 {
   services.adguardhome.enable = true;
   services.adguardhome = {
 
     # General configuration
     host = "127.0.0.1";
+
+    # DNS upstreams
+    settings.dns.upstream_dns =
+      # DNS over HTTPs servers for external resolution
+      [
+        "https://dns.google/dns-query"
+        "https://dns.quad9.net/dns-query"
+        "https://dns.cloudflare.com/dns-query"
+      ]
+      # Resolve hostnames and .internal domains with local DNS
+      ++ [
+        "[//]${GATE}" # resolves hostnames
+        "[/internal/]${GATE}" # resolves .internal domains
+      ]
+      # rDNS for IP hostname resolution
+      ++ [
+        "[/in-addr.arpa/]${GATE}" # resolves IPv4 reverse lookups
+        "[/ip6.arpa/]${GATE}" # resolves IPv6 reverse lookups
+      ];
+    settings.dns.bootstrap_dns =
+      # Cloudflare DNS for initial resolution of upstream DNS servers
+      [ "1.1.1.1" ] ++ [ "1.0.0.1" ] ++ [ "2606:4700:4700::1111" ] ++ [ "2606:4700:4700::1001" ];
+
+    # Advanced DNS features
+    settings.dns.enable_dnssec = true;
+    settings.dns.edns_client_subnet.enabled = true;
+
+    # Performance optimizations
+    settings.dns.ratelimit = 0;
+    settings.dns.upstream_mode = "load_balance";
+    settings.dns.upstream_timeout = "1s";
+    settings.dns.fallback_dns = [ GATE ];
+
+    # Resolve local domains with gateway DNS
+    settings.clients.runtime_sources.rdns = true;
+    settings.dns.use_private_ptr_resolvers = true;
+    settings.dns.local_ptr_upstreams = [ GATE ];
+
+    # Prevent invalid hostname resolution from local machine
+    settings.clients.runtime_sources.hosts = false;
+    settings.dns.hostsfile_enabled = false;
   };
 
   # Nginx reverse proxy to AdGuard-Home with port 3000
