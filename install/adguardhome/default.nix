d5fd70a16dabc6b39491e5d935c7fb4f196d03ba
David E. C. Kopczynski <david.kop.dk@gmail.com>
2026-05-03 20:57:56 +0200

    feat: configured basic-auth for dns.davidkopczynski.com

--- install/adguardhome/default.nix
+++ install/adguardhome/default.nix
@@ -10,16 +10,38 @@
   };
 
   # Nginx reverse proxy to AdGuard-Home with port 3000
-  imports = [ ../nginx/proxy-pass.client.nix ];
+  imports = [ ../nginx/proxy-pass.client.nix ] ++ [ ../nginx/basic-auth.nix ];
 
   services.nginx.enable = true;
   services.nginx.recommendedOptimisation = true;
   services.nginx.virtualHosts."localhost" = {
 
     locations."/" = {
+      extraConfig = config.nginx.basic_auth {
+        authFile = config.sops.secrets."basic-auth/auth".path;
+        tokenFile = config.sops.templates."basic-auth/token".path;
+      };
       proxyPass = "http://${config.services.adguardhome.host}:${toString config.services.adguardhome.port}/";
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  networking.firewall.allowedTCPPorts = [ 53 ] ++ [ 853 ] ++ [ 80 ];
+  networking.firewall.allowedUDPPorts = [ 53 ] ++ [ 853 ];
+
+  # Secrets
+  sops.secrets."basic-auth/auth" = {
+    owner = "nginx";
+    group = "nginx";
+  };
+  sops.secrets."basic-auth/token" = {
+    owner = "nginx";
+    group = "nginx";
+  };
+  sops.templates."basic-auth/token" = {
+    content = ''
+      set  $auth_token "${config.sops.placeholder."basic-auth/token"}";
+    '';
+    owner = "nginx";
+    group = "nginx";
+  };
 }
