22b8510eeabbbac73e3ecf025e383552c9a03ffd
David E. C. Kopczynski <david.kop.dk@gmail.com>
2026-05-14 21:17:43 +0200

    chore: Updated pbs.davidkopczynski.com

--- install/nginx/pbs.davidkopczynski.com/default.nix
+++ install/nginx/pbs.davidkopczynski.com/default.nix
@@ -12,9 +12,10 @@
       extraConfig =
         # Recommended settings from https://pve.proxmox.com/wiki/Web_Interface_Via_Nginx_Proxy
         ''
-          proxy_buffering       off;
-          client_max_body_size  0;
-          send_timeout          ${config.services.nginx.proxyTimeout};
+          proxy_buffering          off;
+          proxy_request_buffering  off;
+          client_max_body_size     0;
+          send_timeout             ${config.services.nginx.proxyTimeout};
         '';
       proxyPass = "https://10.4.10.2:8007/";
       proxyWebsockets = true;
